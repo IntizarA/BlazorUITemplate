1998970423d132039470307ea9a09c1e6a292069
Intizar <intizar.aliyeva69@gmail.com>
2024-08-09 14:51:45 +0400

    Updated resize abilities

--- BlazorUITemplate/BlazorUITemplate/Services/EventService.cs
+++ BlazorUITemplate/BlazorUITemplate/Services/EventService.cs
@@ -1,3 +1,5 @@
+using Microsoft.AspNetCore.Components.Web;
+
 namespace BlazorUITemplate.Services;
 
 public class EventService
@@ -5,18 +7,40 @@ public class EventService
 	public event Func<Task> OnClearAllClick;
 	public event Func<Task> OnRemoveSelectedClick;
 
+	public event Action<MouseEventArgs> OnMouseUp;
+
 	public async Task OnRaiseClearAllEvent ()
 	{
-		if (OnClearAllClick != null)
+		try
 		{
-			await OnClearAllClick.Invoke ();
+			if (OnClearAllClick != null)
+			{
+				await OnClearAllClick.Invoke ();
+			}
+		}
+		catch (Exception exception)
+		{
+			Console.WriteLine (exception.Message);
 		}
 	}
+
 	public async Task OnRaiseRemoveSelectedEvent ()
 	{
-		if (OnRemoveSelectedClick != null)
+		try
 		{
-			await OnRemoveSelectedClick.Invoke ();
+			if (OnRemoveSelectedClick != null)
+			{
+				await OnRemoveSelectedClick.Invoke ();
+			}
 		}
+		catch (Exception exception)
+		{
+			Console.WriteLine (exception.Message);
+		}
+	}
+
+	public void OnMouseUpEvent (MouseEventArgs eventArgs)
+	{
+		OnMouseUp.Invoke (eventArgs);
 	}
 }
